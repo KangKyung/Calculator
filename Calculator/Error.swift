85e283e5b2f40224b5989d97f77eed55e52e8f43
kangkyung <rudgns0508@naver.com>
2021-03-28 11:27:46 +0900

    Feat: Stack 생성
    - Stack struct 생성
    - Stack enum 생성

---
+++ Calculator/Error.swift
@@ -0,0 +1,10 @@
+//
+//  Error.swift
+//  Calculator
+//
+//  Created by 강경훈 on 2021/03/28.
+//
+
+enum StackError: Error {
+  case exceedRangeOfStack
+}
---
+++ Calculator/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Calculator
+//
+//  Created by 강경훈 on 2021/03/28.
+//
+
+import Foundation
+
+print("Hello, World!")
+
