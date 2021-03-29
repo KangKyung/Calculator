f72c0d14981f1cc98514105e88e971b1a907384a
kangkyung <rudgns0508@naver.com>
2021-03-30 08:49:28 +0900

    Feat: 연산자 우선순위 기능추가
    - stack사용 변경: operand1, operand2 -> operand, operator
    - 연산자 우선순위 설정 : 곱셈, 나눗셈 > 덧셈, 뺄셈
    
    \#1: 이진수 연산 미구현
    \#2: 십진수 나눗셈 예외처리 개선필

---
+++ Calculator/Operators/BinaryOperators/CalculateBinaryNumber.swift
@@ -0,0 +1,39 @@
+//
+//  CalculateBinaryNumber.swift
+//  Calculator
+//
+//  Created by 강경 on 2021/03/30.
+//
+
+class CalculateBinaryNumber: CalculateNumber {
+  override func calculateByOperator(_ operator: String,
+                                    _ firstOperand: Int,
+                                    _ secondOperand: Int = 0) throws -> Int {
+    guard let binaryOperator = BinaryOperators(rawValue: `operator`) else {
+      throw CalculateByOperatorError.cantNotMatchStrinngToBinaryOperators
+    }
+
+    switch binaryOperator {
+    case .Plus:
+      return plus(firstOperand, secondOperand)
+    case .Minus:
+      return minus(firstOperand, secondOperand)
+    case .AND:
+      return AND(firstOperand, secondOperand)
+    case .NAND:
+      return NAND(firstOperand, secondOperand)
+    case .OR:
+      return OR(firstOperand, secondOperand)
+    case .NOR:
+      return NOR(firstOperand, secondOperand)
+    case .XOR:
+      return XOR(firstOperand, secondOperand)
+    case .NOT:
+      return NOT(firstOperand)
+    case .LeftShift:
+      return leftShift(firstOperand)
+    case .RightShift:
+      return rightShift(firstOperand)
+    }
+  }
+}
---
+++ Calculator/Operators/DecimalOperators/CalculateDecimalNumber.swift
@@ -0,0 +1,30 @@
+//
+//  CalculateDecimalNumber.swift
+//  Calculator
+//
+//  Created by 강경 on 2021/03/30.
+//
+
+class CalculateDecimalNumber: CalculateNumber {
+  override func calculateByOperator(_ operator: String,
+                                    _ firstOperand: Int,
+                                    _ secondOperand: Int) throws -> Int {
+    guard let decimalOperator = DecimalOperators(rawValue: `operator`) else {
+      throw CalculateByOperatorError.cantNotMatchStrinngToDecimalOperators
+    }
+    
+    switch decimalOperator {
+    case .Plus:
+      return plus(firstOperand, secondOperand)
+    case .Minus:
+      return minus(firstOperand, secondOperand)
+    case .Multiplication:
+      return multiplication(firstOperand, secondOperand)
+    case .Divide:
+      if firstOperand < 1 {
+        throw CalculateByOperatorError.cantNotDivideByZero
+      }
+      return divide(firstOperand, secondOperand)
+    }
+  }
+}
